a0d4d295a45862824aba34e5ff3b0db77731b30b
DevMeteor <2633979287@qq.com>
2020-05-27 09:50:30 +0800

    适配钉钉5.1.1版本

--- app/src/main/java/cn/devmeteor/autodd/service/AService.kt
+++ app/src/main/java/cn/devmeteor/autodd/service/AService.kt
@@ -25,7 +25,7 @@ class AService : AccessibilityService() {
     private var goFill = false
     private var today = false
     private var submit = false
-    private var qqStart=false
+    private var qqStart = false
 
     override fun onInterrupt() {
         Log.i(AppConstant.APP_TAG, "操作失败")
@@ -52,60 +52,63 @@ class AService : AccessibilityService() {
                 }
             }
             if (tabWork && !smartTable) {
-                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0)
-                val waitPos = arrayOf(0, 0, 1, 0, 2, 0, 0, 1)
+                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
+                val waitPos = arrayOf(0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0)
                 click(initPos, waitPos)
             }
             if (smartTable && !fill) {
-                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-                val waitPos = arrayOf(0, 0, 1, 2, 0, 0, 1, 3)
+                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
+                val waitPos = arrayOf(0, 0, 0, 0, 1, 2, 0, 0, 1, 2, 1, 0)
                 click(initPos, waitPos)
             }
             if (fill && !goFill) {
-                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-                val waitPos = arrayOf(0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 4)
+                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
+                val waitPos = arrayOf(0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 4)
                 click(initPos, waitPos)
             }
             if (goFill && !today) {
-                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-                val waitPos = arrayOf(0, 0, 1, 1, 0, 0)
+                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
+                val waitPos = arrayOf(0, 0, 0, 0, 1, 1, 1, 1)
                 click(initPos, waitPos)
             }
             if (today && !submit) {
-                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-                val waitPos = arrayOf(0, 0, 2)
+                val initPos = arrayOf(0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
+                val waitPos = arrayOf(0, 0, 0, 0, 2)
                 click(initPos, waitPos)
             }
-            if (submit&&!qqStart) {
+            if (submit && !qqStart) {
                 performGlobalAction(GLOBAL_ACTION_HOME)
                 Thread.sleep(500)
                 Util.execShell("am force-stop com.alibaba.android.rimet")
-                //有时候能打开QQ但不能进入聊天界面，原因未知
-                val intent=Intent(
+                //小概率能打开QQ但不能进入聊天界面，原因应该出在QQ
+                val intent = Intent(
                     Intent.ACTION_VIEW,
                     //打开好友聊天界面的协议，IgnoreConstant.qqNum是好友QQ号字符串
 //                    Uri.parse("mqqwpa://im/chat?chat_type=wpa&uin=${IgnoreConstant.qqNum}")
                     //打开群聊天界面的协议，IgnoreConstant.groupNum是QQ群号
                     Uri.parse("mqqwpa://im/chat?chat_type=group&uin=${IgnoreConstant.groupNum}")
                 )
-                intent.flags=Intent.FLAG_ACTIVITY_NEW_TASK
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
                 startActivity(intent)
-                qqStart=true
+                qqStart = true
                 Thread.sleep(5000)
             }
         }
-        if (pkgName=="com.tencent.mobileqq"){
-            if (qqStart){
-                val clipboardManager=getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                val clipData=ClipData.newPlainText("label","无异常")
+        if (pkgName == "com.tencent.mobileqq") {
+            if (qqStart) {
+                val clipboardManager =
+                    getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clipData = ClipData.newPlainText("label", "无异常")
                 clipboardManager.setPrimaryClip(clipData)
-                val input=rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mobileqq:id/input")
-                if (input.size!=0) {
+                val input =
+                    rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mobileqq:id/input")
+                if (input.size != 0) {
                     input[0].performAction(AccessibilityNodeInfo.ACTION_CLICK)
                     input[0].performAction(AccessibilityNodeInfo.ACTION_PASTE)
                 }
-                val send=rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mobileqq:id/fun_btn")
-                if (send.size!=0)
+                val send =
+                    rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.tencent.mobileqq:id/fun_btn")
+                if (send.size != 0)
                     send[0].performAction(AccessibilityNodeInfo.ACTION_CLICK)
                 Thread.sleep(1500)
                 performGlobalAction(GLOBAL_ACTION_BACK)
@@ -123,7 +126,7 @@ class AService : AccessibilityService() {
                 goFill = false
                 today = false
                 submit = false
-                qqStart=false
+                qqStart = false
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                     disableSelf()
                 }
